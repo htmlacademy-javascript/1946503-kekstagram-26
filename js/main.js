69baaa38e0133e6552c4b0bbf13dd62e75d2e086
Timur <timurganiev.kzn@mail.ru>
2022-06-19 04:12:12 +0300

    4.15. Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
 const checkStringLength = (string, length) => string.length <= length;
 
-const getRandomNumber = (min, max) => {
-  if (min >= 0 && max >= 0 && min < max) {
-    return Math.round(Math.random() * (max - min) + min);
-  }
-
-  if (min < 0 || max < 0) {
-    return 'минимальное значение должно быть больше нуля';
-  }
-
-  return min;
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
 
-console.log(checkStringLength('', 140));
-console.log(getRandomNumber(1, 5));
-console.log(getRandomNumber(-1, 5));
-console.log(getRandomNumber(1, -5));
-console.log(getRandomNumber(10, 5));
+const id = [getRandomPositiveInteger(1, 25)];
+const url = 'photos/' + [getRandomPositiveInteger(1, 25)] + '.jpg';
+const description = ['Хорошее фото', 'Здорово', 'Могло бы быть лучше', 'Опять еда', 'Всё! Я звоню в полицию!', 'Памагити...'];
+const likes = [getRandomPositiveInteger(15, 200)];
+
+const commentsId = [];
+for (let i = 0; i <= 4; i++) {
+	commentsId[i] = i + 1;
+};
+const commentsAvatar = ['img/avatar-1.svg', 'img/avatar-2.svg', 'img/avatar-3.svg', 'img/avatar-4.svg', 'img/avatar-5.svg', 'img/avatar-6.svg'];
+const commentsMessage = ['Всё отлично!', 'В целом всё неплохо. Но не всё.'];
+const commentsName = ['Артем', 'Вадим', 'Анна', 'Виктор', 'Елена', 'Олег'];
+
+const comments = () => {
+  return {
+    id: commentsId[getRandomPositiveInteger(0, commentsAvatar.length - 1)],
+    avatar: commentsAvatar[getRandomPositiveInteger(0, commentsAvatar.length - 1)],
+    message: commentsMessage[getRandomPositiveInteger(0, commentsMessage.length - 1)],
+    name: commentsName[getRandomPositiveInteger(0, commentsName.length - 1)],
+  };
+};
+
+const similarComments = Array.from({length: 25}, comments);
