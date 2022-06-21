7bcf9f432c64da12ed997854a20f6d036db328e7
Keks <keks@htmlacademy.ru>
2022-06-21 15:53:26 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,5 +1,7 @@
 const checkStringLength = (string, length) => string.length <= length;
 
+checkStringLength('string', 10);
+
 const getRandomPositiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -7,71 +9,96 @@ const getRandomPositiveInteger = (a, b) => {
   return Math.floor(result);
 };
 
-const RANDOM_NUMBERS = {
+const OBJECT_COUNTER = 25;
+
+const MessageCount = {
   min: 1,
-  max: 25,
+  max: 2,
 };
 
-const RANDOM_NUMBERS_LIKES = {
+const LIKES_COUNTER = {
   min: 15,
   max: 200,
 };
 
+const COMMENTS_COUNTER = {
+  min: 0,
+  max: 20,
+};
+
 const AVATARS = [
-'img/avatar-1.svg',
-'img/avatar-2.svg',
-'img/avatar-3.svg',
-'img/avatar-4.svg',
-'img/avatar-5.svg',
-'img/avatar-6.svg',
+  'img/avatar-1.svg',
+  'img/avatar-2.svg',
+  'img/avatar-3.svg',
+  'img/avatar-4.svg',
+  'img/avatar-5.svg',
+  'img/avatar-6.svg',
 ];
 
 const MESSAGES = [
-'Всё отлично!',
-'В целом всё неплохо. Но не всё.',
-'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
 const NAMES = [
-'Артем',
-'Вадим',
-'Анна',
-'Виктор',
-'Елена',
-'Олег',
+  'Артем',
+  'Вадим',
+  'Анна',
+  'Виктор',
+  'Елена',
+  'Олег',
 ];
 
 const DESCRIPTIONS = [
-'Хорошее фото',
-'Здорово',
-'Могло бы быть лучше',
-'Опять еда',
-'Всё! Я звоню в полицию!',
-'Памагити...',
+  'Хорошее фото',
+  'Здорово',
+  'Могло бы быть лучше',
+  'Опять еда',
+  'Всё! Я звоню в полицию!',
+  'Памагити...',
 ];
 
-const getUrl = 'photos/' + [getRandomPositiveInteger(RANDOM_NUMBERS.min, RANDOM_NUMBERS.max)] + '.jpg';
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
-const getComments = () => {
+const createCommentMessage = () => {
+  const messageCount = [getRandomPositiveInteger(MessageCount.min, MessageCount.max)];
+  const message = [];
+
+  for (let i = 1; i <= messageCount; i++) {
+    message.push(MESSAGES[getRandomPositiveInteger(0, MESSAGES.length - 1)]);
+  }
+
+  return [... new Set(message)].join(' ');
+};
+
+let commentsId = 0;
+let photoId = 0;
+
+const createComments = () => {
+  commentsId++;
   return {
-    id: getRandomPositiveInteger(RANDOM_NUMBERS.min, RANDOM_NUMBERS.max),
+    id: commentsId++,
     avatar: AVATARS[getRandomPositiveInteger(0, AVATARS.length - 1)],
-    message: MESSAGES[getRandomPositiveInteger(0, MESSAGES.length - 1)],
+    message: createCommentMessage(),
     name: NAMES[getRandomPositiveInteger(0, NAMES.length - 1)],
   };
 };
 
-const getAllComments = () => {
+const createPhotoDescription = () => {
+  photoId++;
   return {
-    id: getRandomPositiveInteger(RANDOM_NUMBERS.min, RANDOM_NUMBERS.max),
-    url: getUrl,
-    likes: getRandomPositiveInteger(RANDOM_NUMBERS_LIKES.min, RANDOM_NUMBERS_LIKES.max),
-    comments: Array.from({length: [getRandomPositiveInteger(RANDOM_NUMBERS.min, RANDOM_NUMBERS.max)]}, getComments),
+    id: photoId,
+    url: 'photos/' + photoId + '.jpg',
+    likes: getRandomPositiveInteger(LIKES_COUNTER.min, LIKES_COUNTER.max),
+    comments: Array.from({length: [getRandomPositiveInteger(COMMENTS_COUNTER.min, COMMENTS_COUNTER.max)]}, createComments),
     description: DESCRIPTIONS[getRandomPositiveInteger(0, DESCRIPTIONS.length - 1)],
   };
 };
 
-const feedbackPackage = Array.from({length: [getRandomPositiveInteger(RANDOM_NUMBERS.min, RANDOM_NUMBERS.max)]}, getAllComments);
+const createThumbnail = () => Array.from({length: OBJECT_COUNTER}, createPhotoDescription);
+
+createThumbnail();
