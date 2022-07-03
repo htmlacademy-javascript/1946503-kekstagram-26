9ccc52112c490cb864a2611fa7b0635ad412d3e3
Timur <timurganiev.kzn@mail.ru>
2022-07-03 23:43:54 +0300

    7.15. Отрисуй меня полностью (часть ). Исправления

--- js/draw-thumbnail.js
+++ js/draw-thumbnail.js
@@ -1,19 +1,23 @@
 import {createThumbnail} from './data.js';
+import {showBigPicture} from './show-big-picture.js';
 
 const pictureBlock = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
 const pictureData = createThumbnail();
-
 const pictureFragment = document.createDocumentFragment();
 
 const drawThumbnail = () => {
-  pictureData.forEach(({url, likes, comments}) => {
+  pictureData.forEach((picture) => {
     const pictureElement = pictureTemplate.cloneNode(true);
-    pictureElement.querySelector('.picture__img').src = url;
-    pictureElement.querySelector('.picture__likes').textContent = likes.length;
-    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    pictureElement.querySelector('.picture__img').src = picture.url;
+    pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+    pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
     pictureFragment.append(pictureElement);
+
+    pictureElement.addEventListener('click', () => {
+      showBigPicture(picture);
+    });
   });
 
   pictureBlock.append(pictureFragment);
