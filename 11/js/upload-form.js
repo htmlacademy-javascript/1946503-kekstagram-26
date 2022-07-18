18d7d7ab9aed58300c8f5c3b587cefb8159a69e5
Keks <keks@htmlacademy.ru>
2022-07-18 22:52:42 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/upload-form.js
@@ -0,0 +1,111 @@
+import {isEscapeKey} from './util.js';
+import {isUploadFormValid} from './form-validate.js';
+import {onControlSmallerClick, onControlBiggerClick, resetScale} from './scale-image.js';
+import {changeEffect} from './photo-effect.js';
+import {createErrorPopup, createSuccessPopup} from './popup.js';
+import {sendData} from './api.js';
+
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png', 'webp'];
+
+const uploadFile = document.querySelector('#upload-file');
+const imgUploadOverlay = document.querySelector('.img-upload__overlay');
+const uploadCancel = document.querySelector('#upload-cancel');
+const uploadSelectImage = document.querySelector('#upload-select-image');
+const form = document.querySelector('#upload-select-image');
+const buttonUploadElement = document.querySelector('.img-upload__submit');
+const scaleControlSmaller = document.querySelector('.scale__control--smaller');
+const scaleControlBigger = document.querySelector('.scale__control--bigger');
+const imagePreview = document.querySelector('.img-upload__preview img');
+const effectsListElement = document.querySelector('.effects__list');
+const effectLevelFieldset = document.querySelector('.effect-level');
+
+const closeModal = () => {
+  imgUploadOverlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  uploadSelectImage.reset();
+  form.reset();
+  resetScale();
+  imagePreview.style.filter = null;
+  removeEventListener();
+  if (document.querySelector('.pristine-error')) {
+    document.querySelector('.pristine-error').innerHTML = '';
+  }
+};
+
+const openModal = () => {
+  imgUploadOverlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  effectLevelFieldset.classList.add('hidden');
+  addEventListener();
+};
+
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt) && !evt.target.closest('.text__hashtags') && !evt.target.closest('.text__description')) {
+    evt.preventDefault();
+    closeModal();
+  }
+};
+
+const onUploadCancelClick = (evt) => {
+  evt.preventDefault();
+  closeModal();
+};
+
+const onSliderChange = (evt) => {
+  evt.preventDefault();
+  changeEffect(evt.target.value);
+};
+
+function addEventListener() {
+  document.addEventListener('keydown', onDocumentKeydown);
+  uploadCancel.addEventListener('click', onUploadCancelClick);
+  scaleControlSmaller.addEventListener('click', onControlSmallerClick);
+  scaleControlBigger.addEventListener('click', onControlBiggerClick);
+  effectsListElement.addEventListener('change', onSliderChange);
+}
+
+function removeEventListener() {
+  document.removeEventListener('keydown', onDocumentKeydown);
+  uploadCancel.removeEventListener('click', onUploadCancelClick);
+  scaleControlSmaller.removeEventListener('click', onControlSmallerClick);
+  scaleControlBigger.removeEventListener('click', onControlBiggerClick);
+  effectsListElement.removeEventListener('change', onSliderChange);
+}
+
+const onInputUploadFormChange = (evt) => {
+  evt.preventDefault();
+  const file = uploadFile.files[0];
+  const fileName = file.name.toLowerCase();
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+  if (matches) {
+    imagePreview.src = URL.createObjectURL(file);
+    openModal();
+  }
+};
+
+const onSuccess = () => {
+  buttonUploadElement.disabled = false;
+  closeModal();
+  createSuccessPopup();
+};
+
+const onError = () => {
+  buttonUploadElement.disabled = false;
+  createErrorPopup();
+};
+
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  if (isUploadFormValid()) {
+    buttonUploadElement.disabled = true;
+    const formData = new FormData(evt.target);
+    sendData(formData, onSuccess, onError);
+  }
+};
+
+const initUploadFormAction = () => {
+  uploadFile.addEventListener('change', onInputUploadFormChange);
+  form.addEventListener('submit', onFormSubmit);
+};
+
+export {initUploadFormAction};
---
+++ 11/js/util.js
@@ -0,0 +1,44 @@
+const getRandomPositiveInteger = (a, b) => {
+  const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const isArrayUnique = (elements) => {
+  const results = [];
+  elements.forEach((element) => {
+    if (!results.includes(element)) {
+      results.push(element);
+    }
+  });
+  return results.length === elements.length;
+};
+
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+const shuffle = (array) => {
+  const shuffleArray = array.slice();
+  let currentIndex = shuffleArray.length,
+    randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    [shuffleArray[currentIndex], shuffleArray[randomIndex]] = [shuffleArray[randomIndex], shuffleArray[currentIndex]];
+  }
+
+  return shuffleArray;
+};
+
+export {getRandomArrayElement, getRandomPositiveInteger, isEscapeKey, isArrayUnique, debounce, shuffle};
+
